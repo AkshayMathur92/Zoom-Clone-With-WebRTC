0b5b70bb59b84f7162a558767b01924363459bc7
Avi Mathur <avi.mathur.engg@gmail.com>
2020-12-31 18:45:44 +0530

    working condition

--- public/script.js
+++ public/script.js
@@ -9,12 +9,13 @@ myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true
+  audio: false
 }).then(stream => {
-  addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
+    console.log("a call is coming from another peer")
     call.answer(stream)
+    console.log("answerred the call")
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
@@ -22,28 +23,36 @@ navigator.mediaDevices.getUserMedia({
   })
 
   socket.on('user-connected', userId => {
+    console.log("user connected socket", userId)
     connectToNewUser(userId, stream)
   })
+
+  addVideoStream(myVideo, stream)
 })
 
 socket.on('user-disconnected', userId => {
+  console.log('user disconnected' , userId)
   if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
+  console.log("User connected peer", id, ROOM_ID)
   socket.emit('join-room', ROOM_ID, id)
 })
 
 function connectToNewUser(userId, stream) {
+  console.log("connecting to new user by making a call " )
   const call = myPeer.call(userId, stream)
+  console.log("called my peer")
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
+    console.log("call is answered with stream")
     addVideoStream(video, userVideoStream)
   })
   call.on('close', () => {
+    console.log("call is disconnected")
     video.remove()
   })
-
   peers[userId] = call
 }
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidV4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/',  (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
@@ -17,13 +17,20 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    console.log(userId + " joining room " + roomId)
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
-
+    setTimeout(() => {
+      console.log("notifying other users a user is connected")
+      socket.to(roomId).broadcast.emit('user-connected', userId)
+    }, 5000);
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
 })
+
+async function wasteTime(){
+  await new Promise(r => setTimeout(r, 2000));
+}
 
 server.listen(3000)
